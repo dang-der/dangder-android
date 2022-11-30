e1637faec7d6f055b5fec86e5af48d483da5e4b3
hyejineee <dev.hyejin04@gmail.com>
2022-11-30 17:30:35 +0900

    test : LoginViewModel 테스트 케이스 추가
      - 강아지 등록이 되어 있는 경우, 아닌 경우 테스트

--- app/src/test/java/com/viewpoint/dangder/viewmodel/LoginViewModelTest.kt
+++ app/src/test/java/com/viewpoint/dangder/viewmodel/LoginViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.viewpoint.dangder.viewmodel
 
 import com.viewpoint.dangder.action.Actions
+import com.viewpoint.dangder.entity.User
 import com.viewpoint.dangder.usecase.CheckLoggedInUseCase
+import com.viewpoint.dangder.usecase.FetchUserUseCase
 import com.viewpoint.dangder.usecase.LoginUseCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
@@ -13,15 +15,18 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.kotlin.any
 import org.mockito.kotlin.given
+import org.mockito.kotlin.mock
 import timber.log.Timber
 
 class LoginViewModelTest {
 
     private val mockCheckIsLoginUseCase = Mockito.mock(CheckLoggedInUseCase::class.java)
     private val mockLoginUseCase = Mockito.mock(LoginUseCase::class.java)
+    private val mockFetchUserUseCase = Mockito.mock(FetchUserUseCase::class.java)
 
     private lateinit var loginViewModel: LoginViewModel
     private val mainThread = newSingleThreadContext(LoginViewModel::class.java.simpleName)
@@ -29,7 +34,7 @@ class LoginViewModelTest {
     @BeforeEach
     fun setUp() {
         loginViewModel =
-            LoginViewModel(mockCheckIsLoginUseCase, mockLoginUseCase)
+            LoginViewModel(mockCheckIsLoginUseCase, mockLoginUseCase, mockFetchUserUseCase)
         Dispatchers.setMain(mainThread)
     }
 
@@ -49,17 +54,45 @@ class LoginViewModelTest {
             @BeforeEach
             fun setUp() = runTest {
                 given(mockCheckIsLoginUseCase.invoke()).willReturn(true)
+
             }
 
-            @Test
-            @DisplayName("메인 페이지 이동 액션을 발행한다.")
-            fun `it publish GoToMainPage action`() = runTest {
-                loginViewModel.checkIsLogin()
-                loginViewModel.action.subscribe {
-                    Timber.tag("loginViewModel test").d(it.toString())
+            @Nested
+            @DisplayName("강아지 등록이 되어 있는 사용자라면")
+            inner class ContextWithRegisteredDog{
+                @BeforeEach
+                fun setUp() = runTest {
+                    val mockUser = Mockito.mock(User::class.java)
+                    given(mockUser.pet).willReturn(true)
+                    given(mockFetchUserUseCase.invoke()).willReturn(mockUser)
                 }
-                loginViewModel.action.test().awaitCount(1).assertValue(Actions.GoToMainPage)
+
+                @Test
+                @DisplayName("메인 페이지 이동 액션을 발행한다.")
+                fun `it publish GoToMainPage action`() = runTest {
+                    loginViewModel.checkIsLogin()
+                    loginViewModel.action.test().awaitCount(1).assertValue(Actions.GoToMainPage)
+                }
             }
+
+            @Nested
+            @DisplayName("강아지 등록이 되어 있는 않은 사용자라면")
+            inner class ContextWithoutRegisteredDog{
+                @BeforeEach
+                fun setUp() = runTest {
+                    val mockUser = Mockito.mock(User::class.java)
+                    given(mockUser.pet).willReturn(false)
+                    given(mockFetchUserUseCase.invoke()).willReturn(mockUser)
+                }
+
+                @Test
+                @DisplayName("메인 페이지 이동 액션을 발행한다.")
+                fun `it publish GoToMainPage action`() = runTest {
+                    loginViewModel.checkIsLogin()
+                    loginViewModel.action.test().awaitCount(1).assertValue(Actions.GoToInitDogPage)
+                }
+            }
+
         }
 
         @Nested
