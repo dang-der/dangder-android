139172c8a2e80e1f3351823d10c99f5084c0f6c6
hyejineee <dev.hyejin04@gmail.com>
2022-12-02 20:51:46 +0900

    feat : 메인 페이지 카드 모션 기능 구현

---
+++ app/src/main/java/com/viewpoint/dangder/view/data/AroundDog.kt
@@ -0,0 +1,6 @@
+package com.viewpoint.dangder.view.data
+
+data class AroundDog(
+    val id : String,
+    val name :String
+)
--- app/src/main/java/com/viewpoint/dangder/view/main/MainFragment.kt
+++ app/src/main/java/com/viewpoint/dangder/view/main/MainFragment.kt
@@ -1,18 +1,28 @@
 package com.viewpoint.dangder.view.main
 
+import android.view.View
 import com.viewpoint.dangder.R
 import com.viewpoint.dangder.base.BaseFragment
 import com.viewpoint.dangder.databinding.FragmentChatListBinding
 import com.viewpoint.dangder.databinding.FragmentMainBinding
 import com.viewpoint.dangder.databinding.FragmentMyProfileBinding
 import com.viewpoint.dangder.databinding.FragmentTodayBinding
+import com.viewpoint.dangder.view.adapter.AroundDogListAdapter
+import com.viewpoint.dangder.view.data.AroundDog
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
+import com.yuyakaido.android.cardstackview.StackFrom
 
-class MainFragment : BaseFragment<FragmentMainBinding>() {
+class MainFragment : BaseFragment<FragmentMainBinding>(), CardStackListener {
     override val layoutId: Int
         get() = R.layout.fragment_main
+    private val cardStackLayoutManager by lazy { CardStackLayoutManager(requireContext(), this) }
+    private val aroundDogListAdapter by lazy {  AroundDogListAdapter()}
+
 
     override fun initView() {
-
+        initAroundDogList()
     }
 
     override fun subscribeModel() {
@@ -20,6 +30,40 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
     }
 
     override fun initData() {
+
+    }
+
+    private fun initAroundDogList(){
+        binding.mainAroundDogList.layoutManager = cardStackLayoutManager
+        binding.mainAroundDogList.adapter = aroundDogListAdapter
+
+        cardStackLayoutManager.setStackFrom(StackFrom.Top)
+
+        val fakeData = (1..10).map { AroundDog(it.toString(), "name ${it}") }
+        aroundDogListAdapter.submitList(fakeData)
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+
+    }
+
+    override fun onCardRewound() {
+
+    }
+
+    override fun onCardCanceled() {
+
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
 
     }
 }
