7eb6f796c3f040a8af6a733b854b6eacad495270
hyejineee <dev.hyejin04@gmail.com>
2022-12-07 04:08:23 +0900

    feat : 댕더패스구매 다이얼로그 구현

---
+++ app/src/main/java/com/viewpoint/dangder/view/dialog/BuyPassTicketDialog.kt
@@ -0,0 +1,43 @@
+package com.viewpoint.dangder.view.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.iamport.sdk.domain.core.Iamport
+import com.viewpoint.dangder.databinding.DialogBuyPassticketBinding
+import com.viewpoint.dangder.viewmodel.MainViewModel
+
+class BuyPassTicketDialog(
+    private val mainViewModel: MainViewModel
+) : BottomSheetDialogFragment() {
+
+    private lateinit var binding: DialogBuyPassticketBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Iamport.init(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DialogBuyPassticketBinding.inflate(inflater, container, false)
+        handleClickBuyPassTicket()
+        return binding.root
+    }
+
+    private fun handleClickBuyPassTicket() {
+        binding.paymentDialogBuyButton.setOnClickListener {
+            mainViewModel.requestBuyPassTicket(Iamport)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Iamport.close()
+    }
+}
