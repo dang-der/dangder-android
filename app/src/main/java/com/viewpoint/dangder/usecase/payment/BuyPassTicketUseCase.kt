f779903b67f0fc3bd6afcc54290e9b06293db811
hyejineee <dev.hyejin04@gmail.com>
2022-12-06 16:34:28 +0900

    feat : 패스권 구매 유스케이스 생성

---
+++ app/src/main/java/com/viewpoint/dangder/usecase/payment/BuyPassTicketUseCase.kt
@@ -0,0 +1,18 @@
+package com.viewpoint.dangder.usecase.payment
+
+import com.viewpoint.dangder.repository.PaymentRepository
+import javax.inject.Inject
+
+class BuyPassTicketUseCase @Inject constructor(
+    private val paymentRepository: PaymentRepository
+) {
+
+    suspend operator fun invoke(impUid : String, payMoney : Double): Boolean {
+        try {
+            val id = paymentRepository.createPaymentForPassTicket(impUid, payMoney)
+            return id.isNotEmpty()
+        }catch (e : Exception){
+            throw e
+        }
+    }
+}
